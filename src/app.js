6effa5d5d648b751eb0d145cca3e210a961c62a8
John <john_ha80@hotmail.com>
2022-06-16 19:14:36 -0400

    Commit 6/16/2022

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'; 
+
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
@@ -6,3 +8,18 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
